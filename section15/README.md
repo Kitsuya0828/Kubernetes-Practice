8990de82448541b08b7a45eab52c1eef6c6e2a9c
Kitsuya0828 <kitsuyaazuma@gmail.com>
2023-06-29 18:18:29 +0900

    feat: prometheus grafana experiment

---
+++ section15/README.md
@@ -0,0 +1,11 @@
+Create
+```bash
+kind create cluster --config kind.yaml --name kindcluster
+```
+
+Delete
+```bash
+kind delete cluster --name kindcluster
+```
+
+[Quick Start \- Prometheus Operator](https://prometheus-operator.dev/docs/prologue/quick-start/)
---
+++ section15/tutorial.sh
@@ -0,0 +1,17 @@
+git clone https://github.com/prometheus-operator/kube-prometheus.git
+
+# Create the namespace and CRDs, and then wait for them to be availble before creating the remaining resources
+kubectl create -f manifests/setup
+
+# Wait until the "servicemonitors" CRD is created. The message "No resources found" means success in this context.
+until kubectl get servicemonitors --all-namespaces ; do date; sleep 1; echo ""; done
+
+kubectl create -f manifests/
+
+
+kubectl --namespace monitoring port-forward svc/prometheus-k8s 9090
+kubectl --namespace monitoring port-forward svc/alertmanager-main 9093
+kubectl --namespace monitoring port-forward svc/grafana 3000
+
+
+kubectl delete --ignore-not-found=true -f manifests/ -f manifests/setup
